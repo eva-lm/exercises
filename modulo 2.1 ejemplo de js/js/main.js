0b9a74360e034ce950bec676e9e056f66514f71d
Miguel del Mazo <migueldelmazo@gmail.com>
2019-07-18 18:41:22 +0200

    Add exercise m2.1

---
+++ modulo 2.1 ejemplo de js/js/main.js
@@ -0,0 +1,32 @@
+"use strict";
+
+const sum = function() {
+  debugger;
+  // get numberA value
+  let numberAValue = numberA.value;
+  // parse string value to number value
+  numberAValue = parseInt(numberAValue);
+
+  // get numberB value
+  let numberBValue;
+  numberBValue = numberB.value;
+  // parse string value to number value
+  numberBValue = parseInt(numberBValue);
+
+  // add values
+  const resultValue = numberAValue + numberBValue;
+  // set result value in result input element
+  result.value = resultValue;
+};
+
+// get numberA dom element
+const numberA = document.querySelector(".numberA");
+// get numberB dom element
+const numberB = document.querySelector(".numberB");
+// get result dom element
+const result = document.querySelector(".result");
+
+// listen changes in numberA input
+numberA.addEventListener("change", sum);
+// listen changes in numberA input
+numberB.addEventListener("change", sum);
