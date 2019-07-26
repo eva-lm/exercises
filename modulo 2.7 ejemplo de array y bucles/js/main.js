2c5a5310b6911d1b2002500b6eab050eba004539
Miguel del Mazo <migueldelmazo@gmail.com>
2019-07-26 16:55:49 +0200

    Update m2.7 example

--- modulo 2.7 ejemplo de array y bucles/js/main.js
+++ modulo 2.7 ejemplo de array y bucles/js/main.js
@@ -1,38 +1,71 @@
-'use strict';
+"use strict";
 
 const students = [
-  'Amanda Elena Padilla Santiago',
-  'Ana Amaro Vázquez',
-  'Ana Montiaga García',
-  'Bárbara Chaves Barcellos',
-  'Beatriz del Valle',
-  'Eva Lorenzo Mendieta',
-  'Isabel Pizarro Nogales',
-  'Laura Sánchez Martín',
-  'Laura Pareja Almodóvar',
-  'Laura Ruiz Mazorra',
-  'Luz de Belén Aguirre Morales',
-  'Maria Sola Hornedo',
-  'María del Mar Prieto Nieto',
-  'Raquel Montilla Calvo',
-  'Raquel Méndiz Mateo',
-  'Samantha Cancino Cofré',
-  'Sandra Cubero Rosado',
-  'Sara Martin Gutierrez',
-  'Sara Marín Pino',
-  'Tamara Leticia Sánchez Orellana'
+  "Amanda Elena Padilla Santiago",
+  "Ana Amaro Vázquez",
+  "Ana Montiaga García",
+  "Bárbara Chaves Barcellos",
+  "Beatriz del Valle",
+  "Eva Lorenzo Mendieta",
+  "Isabel Pizarro Nogales",
+  "Laura Sánchez Martín",
+  "Laura Pareja Almodóvar",
+  "Laura Ruiz Mazorra",
+  "Luz de Belén Aguirre Morales",
+  "Maria Sola Hornedo",
+  "María del Mar Prieto Nieto",
+  "Raquel Montilla Calvo",
+  "Raquel Méndiz Mateo",
+  "Samantha Cancino Cofré",
+  "Sandra Cubero Rosado",
+  "Sara Martin Gutierrez",
+  "Sara Marín Pino",
+  "Tamara Leticia Sánchez Orellana"
 ];
 
-// students[3] = 'Ada Lovelace';
-
 let info;
-let ulContent = '';
+let i;
 
-for (let i = 0; i < 20; i = i + 1) {
-  info = 'Student #' + i + ': ' + students[i];
+for (const student of students) {
+  info = "Student" + ": " + student;
   console.log(info);
-  ulContent = ulContent + '<li>' + info + '</li>';
 }
+console.log("--------------");
 
-const ulElement = document.querySelector('.js-list');
-ulElement.innerHTML = ulContent;
+for (i = 0; i < 20; i = i + 1) {
+  info = "Student #" + i + ": " + students[i];
+  console.log(info);
+}
+console.log("--------------");
+
+for (i = 19; i >= 0; i = i - 1) {
+  info = "Student #" + i + ": " + students[i];
+  console.log(info);
+}
+console.log("--------------");
+
+for (i = 0; i < 20; i = i + 2) {
+  info = "Student #" + i + ": " + students[i];
+  console.log(info);
+}
+console.log("--------------");
+
+for (i = 10; i < 20; i = i + 1) {
+  info = "Student #" + i + ": " + students[i];
+  console.log(info);
+}
+console.log("--------------");
+
+for (i = 0; i <= students.length; i = i + 1) {
+  info = "Student #" + i + ": " + students[i];
+  console.log(info);
+}
+console.log("--------------");
+
+for (let student = 0; student < students.length; student = student + 1) {
+  let words = students[student].split(" ");
+  for (let word = 0; word < words.length; word = word + 1) {
+    info = "Word #" + word + " of: " + students[student] + " is: " + words[word];
+    console.log(info);
+  }
+}
