96a2a9f5a46c57266e5a8f4d377a8400f4dadd1c
info@migueldelmazo.com <migueldelmazo@gmail.com>
2019-08-02 18:28:12 +0200

    Add 2.11 localstorage example

---
+++ module 2.11 example localstorage/js/main.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const form = document.querySelector('.js-form');
+const nameInput = document.querySelector('.js-name');
+const surnameInput = document.querySelector('.js-surname');
+
+const getFromLocalStorage = () => {
+  const userData = JSON.parse(localStorage.getItem('userData'));
+  nameInput.value = userData.name;
+  surnameInput.value = userData.surname;
+}
+
+const handleInput = () => {
+  const data = {
+    name: nameInput.value,
+    surname: surnameInput.value
+  };
+  localStorage.setItem('userData', JSON.stringify(data));
+}
+
+form.addEventListener('keyup', handleInput);
+
+getFromLocalStorage();
