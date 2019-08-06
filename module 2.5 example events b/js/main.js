cdd08d69bce533a68506c8fb6adbab1cbdd7d8c5
info@migueldelmazo.com <migueldelmazo@gmail.com>
2019-08-06 12:00:37 +0200

    Improve event example

--- module 2.5 example events b/js/main.js
+++ module 2.5 example events b/js/main.js
@@ -1,20 +1,28 @@
 'use strict';
 
-const handleContainerClick = (ev) => {
+// container
+
+const handleContainerClick = ev => {
   console.log('handleContainerClick: el elemento pulsado es', ev.target);
   console.log('handleContainerClick: el elemento escuchado es', ev.currentTarget);
-}
+  ev.preventDefault();
+};
 
-const handleLinkClick = (ev) => {
+const container = document.querySelector('.js-container');
+container.addEventListener('click', handleContainerClick);
+
+// links
+
+const handleLinkClick = ev => {
   console.log('handleLinkClick: el elemento pulsado es', ev.target);
   console.log('handleLinkClick: el elemento escuchado es', ev.currentTarget);
 
   ev.preventDefault(); // des/comentar esta linea a ver qué pasa
-  ev.stopPropagation(); // des/comentar esta linea a ver qué pasa
-}
+  // ev.stopPropagation(); // des/comentar esta linea a ver qué pasa
+};
 
-const container = document.querySelector('.js-container');
-const link = document.querySelector('.js-link');
+const linkAdalab = document.querySelector('.js-link-adalab');
+linkAdalab.addEventListener('click', handleLinkClick);
 
-container.addEventListener('click', handleContainerClick);
-link.addEventListener('click', handleLinkClick);
+const linkGoogle = document.querySelector('.js-link-google');
+linkGoogle.addEventListener('click', handleLinkClick);
